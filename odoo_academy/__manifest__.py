31363341d9043068390a1a63b2fe278aaf105fe7
Paco71312 <ing.josearjona@gmail.com>
2022-09-26 20:46:30 +0000

    [ADD] Basic Module

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -17,7 +17,7 @@
     'category': 'Training',
     'version': '0.1',
     'depends': ['base'],
-    'data':['security/academy_security.xml','security/ir.model.access.csv','views/academy_menuitems.xml','views/course_views.xml',
+    'data':['security/academy_security.xml','security/ir.model.access.csv','views/academy_menuitems.xml','views/course_views.xml','views/session_views.xml',
 
     ],
     'demo':['demo/academy_demo.xml',
