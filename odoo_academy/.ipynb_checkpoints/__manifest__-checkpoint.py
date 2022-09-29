9faa2ae53527ed665c17ecaaeb3991fa009a9d1c
Paco71312 <ing.josearjona@gmail.com>
2022-09-29 14:53:23 +0000

    [ADD] Basic Module

--- odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -17,7 +17,7 @@
     'category': 'Training',
     'version': '0.1',
     'depends': ['sale',],
-    'data':['security/academy_security.xml','security/ir.model.access.csv','views/academy_menuitems.xml','views/course_views.xml','views/session_views.xml','views/sales_views_inherit.xml'
+    'data':['security/academy_security.xml','security/ir.model.access.csv','views/academy_menuitems.xml','views/course_views.xml','views/session_views.xml','views/sales_views_inherit.xml','views/product_views_inherit.xml','wizard/sale_wizard_view.xml',
 
     ],
     'demo':['demo/academy_demo.xml',
